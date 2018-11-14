bde94c03c2afd917f690717a1e08edd8dcbf5c6d
Muhammad Nuzulul Ilham <33631317+zulhaamid@users.noreply.github.com>
2018-11-14 09:01:48 +0700

    Update kkl.php

--- inc/kkl.php
+++ inc/kkl.php
@@ -4,16 +4,16 @@
         <td height="94" colspan="3"><img src="kkl/site.png" width="711" height="91"></td>
       </tr>
       <tr>
-        <td width="578" rowspan="6"><img src="kkl/utama.jpg" name="utama" width="572" height="712" id="utama"></td>
-        <td width="62" height="52"><img src="kkl/1.jpg" width="60" height="60" onMouseOver="MM_swapImage('utama','','kkl/1.jpg',1)" onMouseOut="MM_swapImgRestore()"></td>
-        <td width="67"><img src="kkl/2.jpg" width="69" height="60" onMouseOver="MM_swapImage('utama','','kkl/2.jpg',1)" onMouseOut="MM_swapImgRestore()"></td>
+        <td width="578" rowspan="6"><img src="kkl/utama.jpeg" name="utama" width="572" height="712" id="utama"></td>
+        <td width="62" height="52"><img src="kkl/1.jpeg" width="60" height="60" onMouseOver="MM_swapImage('utama','','kkl/1.jpeg',1)" onMouseOut="MM_swapImgRestore()"></td>
+        <td width="67"><img src="kkl/2.jpeg" width="69" height="60" onMouseOver="MM_swapImage('utama','','kkl/2.jpeg',1)" onMouseOut="MM_swapImgRestore()"></td>
       </tr>
       <tr>
-        <td height="49"><img src="kkl/3.jpg" width="60" height="60" onMouseOver="MM_swapImage('utama','','kkl/3.jpg',1)" onMouseOut="MM_swapImgRestore()"></td>
-        <td><img src="kkl/4.JPG" width="69" height="60" onMouseOver="MM_swapImage('utama','','kkl/4.JPG',1)" onMouseOut="MM_swapImgRestore()"></td>
+        <td height="49"><img src="kkl/3.jpeg" width="60" height="60" onMouseOver="MM_swapImage('utama','','kkl/3.jpeg',1)" onMouseOut="MM_swapImgRestore()"></td>
+        <td><img src="kkl/4.jpeg" width="69" height="60" onMouseOver="MM_swapImage('utama','','kkl/4.jpeg',1)" onMouseOut="MM_swapImgRestore()"></td>
       </tr>
       <tr>
-        <td height="22" onMouseOver="MM_swapImage('utama','','kkl/5.JPG',1)"><img src="kkl/5.JPG" width="60" height="60"></td>
-        <td><img src="kkl/6.jpg" width="70" height="60" onMouseOver="MM_swapImage('utama','','kkl/6.jpg',1)" onMouseOut="MM_swapImgRestore()"></td>
+        <td height="22" onMouseOver="MM_swapImage('utama','','kkl/5.jpeg',1)"><img src="kkl/5.jpeg" width="60" height="60"></td>
+        <td><img src="kkl/6.jpeg" width="70" height="60" onMouseOver="MM_swapImage('utama','','kkl/6.jpeg',1)" onMouseOut="MM_swapImgRestore()"></td>
       </tr>
     </table>
